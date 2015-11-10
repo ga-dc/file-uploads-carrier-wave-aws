e02312f105cda83f9a4cd7eac1c06f4401d8577e
Jesse Shawl <jesse@jshawl.com>
2015-11-10 16:26:33 -0500

    Image crud via image url

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Image.destroy_all
+Image.create([
+  {image_url: "https://upload.wikimedia.org/wikipedia/en/thumb/e/e9/Ruby_on_Rails.svg/791px-Ruby_on_Rails.svg.png"},
+  {image_url: "https://i.ytimg.com/vi/aHqnFWLP7wA/maxresdefault.jpg"},
+  {image_url: "http://www.kylembrown.com/wp-content/uploads/2013/11/logo.png"},
+  {image_url: "http://ickosovo.com/wp-content/uploads/2013/07/python-django.png"}
+])
